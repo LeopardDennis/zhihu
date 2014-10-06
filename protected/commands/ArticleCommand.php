2495fcddb53276589abc4de369518f2d4f64f2c5
Dennis Yu <forever88959@gmail.com>
2014-10-06 14:23:34 +0800

    Cronjob

--- protected/commands/ArticleCommand.php
+++ protected/commands/ArticleCommand.php
@@ -19,4 +19,29 @@ class ArticleCommand extends CConsoleCommand
 		
 		Article::model()->saveBeforeArticles($date);
 	}
+
+	public function actionGetAllArticles()
+	{
+		for($i = 0; $i <= 5000000; $i++)
+		{
+			$url = 'http://news-at.zhihu.com/api/3/news/'.$i;
+			$curl = curl_init();
+			curl_setopt($curl, CURLOPT_URL, $url);
+			curl_setopt($curl, CURLOPT_HEADER, 0);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+			$output = curl_exec($curl);
+			$httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+			curl_close($curl);
+
+			$newsIds = new NewsIds();
+			$newsIds->news_id = $i;
+			$newsIds->http_code = $httpCode;
+			$newsIds->created = strtotime('today');
+			$res = $newsIds->save();
+			$output = $res ? 'Success' : 'Failure';
+
+			echo 'News ID: '.$i.' '.$httpCode.' '.$output."\n";
+		}
+	}
 }
